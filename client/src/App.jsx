4eba9bd59b587215069a7b0af62cf23998a59c09
APURV SINHA <119706011+xeylous@users.noreply.github.com>
2026-01-16 19:01:19 +0530

    smmoth scroll added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,37 @@ import KodermaWildlifeSanctuary from "./components/KodermaWildlifeSanctuary/Kode
 import MaaChanchalaDeviMandir from "./components/MaaChanchalaDeviMandir/MaaChanchalaDeviMandir.jsx";
 
 
+
+import { useEffect } from "react";
+import Lenis from "lenis";
+
 const App = () => {
+  useEffect(() => {
+    // Initialize Lenis
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // Custom easing for premium feel
+      direction: 'vertical',
+      gestureDirection: 'vertical',
+      smooth: true,
+      mouseMultiplier: 1,
+      smoothTouch: false,
+      touchMultiplier: 2,
+    });
+
+    // RAF loop
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return (
     <>
    
